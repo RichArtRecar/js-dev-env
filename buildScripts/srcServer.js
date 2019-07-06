ddb167879b86a0c0f252ab28ab3c7b439685855a
Rich Recar <richard_recar@outlook.com>
2019-07-06 16:50:40 -0400

    'nsp depricated'

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -13,12 +13,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
 	publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-	if (err){
+app.listen(port, function (err) {
+	if (err) {
+		// eslint-disable-next-line no-console
 		console.log('err');
 	} else {
 		open('http://localhost:' + port);
